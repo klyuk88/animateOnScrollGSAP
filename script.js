b5603fc2aeb0575a838bca85828505f0e385719d
Ilya Klyukovskiy <48031577+klyuk88@users.noreply.github.com>
2021-10-16 10:32:06 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,75 @@
+    function animateOnScroll() {
+
+        const fadeUpElems = document.querySelectorAll('.fadeUpElem')
+        const fadeInElems = document.querySelectorAll('.fadeInElem')
+        const lineElems = document.querySelectorAll('.lineElem')
+
+        function width() {
+            if (window.innerWidth <= 1220) {
+                return 'top bottom'
+            } else {
+                return '30% 90%'
+            }
+        }
+
+
+        function hide(elem) {
+            gsap.set(elem, { autoAlpha: 0 });
+        }
+
+        gsap.utils.toArray(fadeUpElems).forEach(item => {
+            hide(item);
+            ScrollTrigger.create({
+                trigger: item,
+                start: width(),
+                onEnter: function () {
+                    gsap.fromTo(item, {
+                        y: 70
+                    }, {
+                        autoAlpha: 1,
+                        duration: 1.5,
+                        y: 0,
+                        ease: 'expo',
+                    })
+                },
+                once: true
+            })
+
+        })
+        gsap.utils.toArray(fadeInElems).forEach(item => {
+            hide(item);
+            ScrollTrigger.create({
+                trigger: item,
+                onEnter: function () {
+                    gsap.to(item, {
+                        autoAlpha: 1,
+                        duration: 1.5,
+                        ease: 'expo'
+                    })
+                },
+                once: true
+            })
+
+        })
+        gsap.utils.toArray(lineElems).forEach(item => {
+            hide(item);
+            gsap.set(item, { width: 0 })
+            ScrollTrigger.create({
+                trigger: item,
+                onEnter: function () {
+                    gsap.to(item, {
+                        width: '100%',
+                        autoAlpha: 1,
+                        duration: 0.5,
+                        ease: 'power1.in',
+                    })
+
+                },
+                once: true
+            })
+
+        })
+
+    }
+
+    animateOnScroll();
